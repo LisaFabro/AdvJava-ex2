b0d7356f23a9cb8b5d2e071966c525b78b5627db
LisaFabro <fabro.lisa@gmail.com>
2023-11-15 16:57:06 +0100

    add es 2 corretto

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     //Scrivere una funzione che controlli se un carattere è un numero altrimenti lanciare una eccezione.
-    public static void main(String[] args){
-        char input = '&';
+    public static void main(String[] args) {
+        char input = '1';
         check(input);
     }
-    public static void check(char a){
-        try{
-            if(Character.isDigit(a)){
-                System.out.println(a + " è un numero");
-            } else {
-                throw new Exception();
-            };
-        }catch (Exception e){
+
+    public static void check(char a) {
+        try {
+            Integer.parseInt(String.valueOf(a));
+            System.out.println(a + " è un numero");
+
+        } catch (Exception e) {
             System.out.println(a + " non è un numero");
         }
     }
